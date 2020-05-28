da64ee5438c98a173ea4e6fc05d286ea4a441fda
Thijs Lausegger <Thijs.lausegger@gmail.com>
2020-05-28 10:24:09 +0200

    first commit

---
+++ navbar.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="./css/navbar.css">
+
+<div class="mynav">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#" id="logo">Buddy<span>Fat</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php?content=home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=food">Food</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=exercises">Exercises</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=faq">FAQ</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav mr-auto Login">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=signup">Signup</a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">|</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?content=login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
